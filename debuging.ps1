decaca09fbe1422b7353b0f8911509052a214b43
Mark van Hattem <mvhattemsysops@outlook.com>
2023-05-04 15:14:30 +0200

    voorbeeld-debug prog zonder fouten

--- debuging.ps1
+++ debuging.ps1
@@ -8,7 +8,7 @@ function Start-RPSGame {
 
     while($Round -le $NoOfRounds)
         {
-            for ($RPSCounterDisp = 1; $RPSCounterDisp -lt 3; $RPSCounterDisp++)
+            for ($RPSCounterDisp = 1; $RPSCounterDisp -le 3; $RPSCounterDisp++){
                 switch ($RPSCounterDisp){
                     1 {write-output "Rock"}
                     2 {write-output "Paper"}
@@ -16,26 +16,24 @@ function Start-RPSGame {
                 }
             Start-Sleep -Milliseconds 500
             Write-OutRPSOption
+            $Round++
+            Write-output "`n`n"
+            Start-Sleep -Seconds 2
+            }
         }
 
     }
 
 
 function Write-OutRPSOption {
-    $RPSChoice = get-rando -Minimum 1 -Maximum 9
-    write-verobse "Random number was $RPSChoice"
+    $RPSChoice = get-random -Minimum 1 -Maximum 9
+    write-verbose "Random number was $RPSChoice"
     switch ($RPSChoice) {
-        {PSItem -gt 1 -and $PSItem -lt 3} {write-output "** ROCK **"}
-        {PSItem -gt 4 -and $PSItem -lt 6} {write-output "** PAPER **"}
-        {PSItem -gt 7 -and $PSItem -lt 9} {write-output "** SCISSORS **"}
+        {$PSItem -ge 1 -and $PSItem -le 3} {write-output "** ROCK **"}
+        {$PSItem -ge 4 -and $PSItem -le 6} {write-output "** PAPER **"}
+        {$PSItem -ge 7 -and $PSItem -le 9} {write-output "** SCISSORS **"}
     }
 }
-
-
-
-
-
-
 
 
 #main execution block
